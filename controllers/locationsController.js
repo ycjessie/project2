2d555cb4a7351c72e9aaf6f902db291c62229692
Jessie Chen <yhcjessie1@gmail.com>
2020-11-03 02:28:21 -0500

    try to add second location but not working

--- controllers/locationsController.js
+++ controllers/locationsController.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const dayjs = require("dayjs");
 const Location = require('../models/location').Location;
 const Address = require('../models/location').Address;
 
@@ -8,17 +9,32 @@ router.get('/new', (req, res) => {
 });
 // ADD EMPTY FORM TO Location SHOW PAGE TO ADD address TO A location
 router.get("/:locationId", (req, res) => {
-  // find location in db by id and add new address
   let now = dayjs(Location.createdAt);
-  Location.findById(req.params.userId, (error, locaton) => {
-    res.render("users/show.ejs", { location, now });
+  Location.findById(req.params.locationId, (error, location) => {
+    res.render("locations/show.ejs", { location, now });
   });
 });
 // CREATE A NEW LOCATION
 router.post('/', (req, res) => {
   Location.create(req.body, (error, newLocation) => {
-    //res.send(newLocation);
-    res.redirect(`/user/${newLocation._id}`);
+    res.send(newLocation);
+    //res.redirect(`/locations/${newLocation._id}`);
+  });
+});
+
+// CREATE Address EMBEDDED IN location
+router.post("/:locationId/address", (req, res) => {
+  console.log(req.body);
+  // store new address in memory with data from request body
+  const newAddress = new Address({ cityName:req.body.cityName,stateName:req.body.stateName});
+
+  // find location in db by id and add new address
+  Location.findById(req.params.locationId, (error, location) => {
+    location.address.push(newAddress);
+    location.save((err, location) => {
+      //res.send(location)
+      res.redirect(`/locations/${location.id}`);
+    });
   });
 });
 
--- models/location.js
+++ models/location.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
 
 const addressSchema = new mongoose.Schema({
-  streetName:{type: String,unique:true,require:true},
-  cityName:{type: String,unique:true,require:true},
-  stateName:{type: String,unique:true,require:true},
-  zipCode:{type: String,unique:true,require:true},
+  cityName:{type:String,unique:true},
+  stateName:{type:String,unique:true},
+ // stateName:{type: String,unique:true,require:true},
 },{timestamps:true});
 
 const locationSchema = new mongoose.Schema({
-  name: {type: String, unique:true,require:true},
+  name: String,
   // embed address in location
   address: [addressSchema],
 },{timestamps:true});
